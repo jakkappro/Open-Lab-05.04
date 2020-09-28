ea38a0005564468f63ca9a32fdf254fd34577b02
jakub kapitulcin <kapitulcinjakub1@gmail.com>
2020-09-28 20:46:07 +0200

    I made function to remove special characters.

--- Open-Lab-05.04/StringTools.cs
+++ Open-Lab-05.04/StringTools.cs
@@ -6,7 +6,24 @@ namespace Open_Lab_05._04
     {
         public string RemoveSpecialCharacters(string original)
         {
-            throw new NotImplementedException();
+            string ret = "";
+            string[] specialChar = {"!", "@", "#", "$", "%", "%", "^", "&", "*", "(", ")", "=", "+", "[", "{", "]", "}", ";", "\"", "'", "|", "\\", ",", "<", ">", ".", "/", "?", "~", "`", ":" };
+            bool canWrite = true;
+
+            for (int i = 0; i < original.Length; i++)
+            {
+                canWrite = true;
+                for( int y = 0; y < specialChar.Length; y++)
+                {
+                    if( specialChar[y] == original.ToCharArray()[i].ToString())
+                    {
+                        canWrite = false;
+                        break;
+                    }
+                }
+                if(canWrite) ret += original.ToCharArray()[i].ToString();
+            }
+            return ret;
         }
     }
 }
